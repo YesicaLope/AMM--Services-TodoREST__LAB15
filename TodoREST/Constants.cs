d18ffbfd90a6b2ae9d032b93f19183c4845f1666
YesicaLope <yesica.lope@tecsup.edu.pe>
2022-07-04 11:50:42 -0500

    first commit

---
+++ TodoREST/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TodoREST
+{
+    public static class Constants
+    {
+        public static string RestUrl = "http://18.232.226.120/TodoAPI/api/todoitems/{0}";
+
+
+
+    }
+}
